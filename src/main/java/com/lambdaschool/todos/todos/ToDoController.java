67558306848fc6913b45ec8a9d76b0eb4c6c98a3
Danny Vail <danny_vail@outlook.com>
2019-02-22 15:04:53 -0500

    EXPOSE GET ENDPOINTS FOR TODO In UserToDo.java - Create an interface based projection to include the user name with the todo data
    In ToDoRepository.java
    - Add a named query to find all todos and apply the UserToDo projection
    - Add a named query to find todos by completed status
    
    In ToDoController.java
    - Expose GET /todos to return all todos
    - Expose GET /todos/todoid/{todoid} to return a todo based on the todo id
    - Expose GET /todos/users to return all todos with the assigned user name
    - Expose GET /todos/active to return all todos still pending

--- src/main/java/com/lambdaschool/todos/todos/ToDoController.java
+++ src/main/java/com/lambdaschool/todos/todos/ToDoController.java
@@ -1,9 +1,14 @@
 package com.lambdaschool.todos.todos;
 
+import com.lambdaschool.todos.todos.projections.UserToDo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * ToDo REST Controller
@@ -13,4 +18,47 @@ import org.springframework.web.bind.annotation.RestController;
 public class ToDoController {
   @Autowired
   private ToDoRepository toDoRepo;
+
+  private static final int TODO_PENDING = 0;
+  private static final int TODO_COMPLETE = 1;
+
+  /**
+   * Find all todo items.
+   *
+   * @return  A list of all to do items
+   */
+  @GetMapping()
+  public List<ToDo> findAllToDos() {
+    return toDoRepo.findAll();
+  }
+
+  /**
+   * Find a todo item by id
+   * @param toDoId  A todo id
+   * @return        A todo item or null if todo is not found
+   */
+  @GetMapping("/todoid/{toDoId}")
+  public ToDo findToDoById(@PathVariable long toDoId) {
+    return toDoRepo.findById(toDoId).orElse(null);
+  }
+
+  /**
+   * Find all todos with the assigned user name.
+   *
+   * @return  A list of all todos with the assigned user name
+   */
+  @GetMapping("/users")
+  public List<UserToDo> findAllUserToDos() {
+    return toDoRepo.findUserToDosBy();
+  }
+
+  /**
+   * Find all todos not yet completed.
+   *
+   * @return  A list of pending todos
+   */
+  @GetMapping("/active")
+  public List<ToDo> findPendingToDos() {
+    return toDoRepo.findByCompletedEquals(TODO_PENDING);
+  }
 }
